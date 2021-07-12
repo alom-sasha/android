98943ceeb867438e34dc48920d493d6b898f776a
haiseong <wjdgotjd9908@gmail.com>
2021-07-13 00:40:57 +0900

    sms기능 추가
    AVD환경이라 그런지 sms보내는 함수 실행시 앱이 종료됨

--- app/src/main/java/com/example/sasha/report.java
+++ app/src/main/java/com/example/sasha/report.java
@@ -2,30 +2,39 @@ package com.example.sasha;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.telephony.SmsManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+
 public class report extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report);
 
-        Button b = (Button)findViewById(R.id.button_report);
-        b.setOnLongClickListener(new View.OnLongClickListener() {
+        final String phoneNumber = "010-5444-7857";
+        final String message = "신고합니다 ~~~~"; // 현제 위치등 정보를 문자로 신고
+        Button button_report = (Button)findViewById(R.id.button_report);
+        button_report.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
             public boolean onLongClick(View v) {
                 // 오랫동안 눌렀을 때 이벤트가 발생됨
                 Toast.makeText(getApplicationContext(),
                         "신고하려면 아래로 스와이프 해주세요.", Toast.LENGTH_SHORT).show();
+                // 스와이프 하는 코드
 
-                // 리턴값이 있다
-                // 이메서드에서 이벤트에대한 처리를 끝냈음
-                // 그래서 다른데서는 처리할 필요없음 true
-                // 여기서 이벤트 처리를 못했을 경우는 false
+                // 신고 문자 보내기
+                SmsManager sms = SmsManager.getDefault();
+                sms.sendTextMessage(phoneNumber, null, message, null, null);
+
+
                 return true;
             }
 
