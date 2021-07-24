32a6f5f26a6cd931801efd03791bba7313aa1656
geonwoo <78127896+bokko7711@users.noreply.github.com>
2021-07-24 16:03:52 +0900

    firebase connected

--- app/src/main/java/com/example/sasha/AlertPageFragment.java
+++ app/src/main/java/com/example/sasha/AlertPageFragment.java
@@ -16,7 +16,6 @@ import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
-import com.naver.maps.map.MapView;
 
 public class AlertPageFragment extends Fragment {
 
