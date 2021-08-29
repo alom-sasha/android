4db39d9afc320ec236754be6e9b213e7f6436ceb
haiseong <wjdgotjd9908@gmail.com>
2021-08-30 08:48:03 +0900

    aa

--- app/src/main/java/com/example/sasha/SetOriginDestinationFregment.java
+++ app/src/main/java/com/example/sasha/SetOriginDestinationFregment.java
@@ -8,13 +8,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class SetOriginFregment extends Fragment {
+public class SetOriginDestinationFregment extends Fragment {
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_set_origin_page, container, false);
+        return inflater.inflate(R.layout.fragment_set_origin_destination_page, container, false);
     }
 }
