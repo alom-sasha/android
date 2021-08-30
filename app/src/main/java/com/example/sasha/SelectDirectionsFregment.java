4116d58c82c86ac8ec60528901e90c8ca4c60217
haiseong <wjdgotjd9908@gmail.com>
2021-08-30 10:19:09 +0900

    s

--- app/src/main/java/com/example/sasha/SelectDirectionsFregment.java
+++ app/src/main/java/com/example/sasha/SelectDirectionsFregment.java
@@ -1,18 +1,12 @@
 package com.example.sasha;
 
-import android.app.Activity;
 import android.os.Bundle;
 
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.TextView;
-
-import com.naver.maps.map.overlay.Marker;
 
 
 public class SelectDirectionsFregment extends Fragment {
@@ -22,20 +16,6 @@ public class SelectDirectionsFregment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-
-        ViewGroup rootView = (ViewGroup)inflater.inflate(R.layout.fragment_select_directions_page, container, false);
-
-        TextView cancel_button = (TextView) rootView.findViewById(R.id.cancel_button);
-        cancel_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // 길찾기 액비티비 종
-            }
-        });
-
-
-
-        return rootView;
+        return inflater.inflate(R.layout.fragment_select_directions_page, container, false);
     }
-
 }
